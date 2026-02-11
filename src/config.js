41b7d5ad488439c99fa41d8604411d47739a1a97
Muhammed Rashid V <rashid@godlandinfotech.com>
2026-02-11 14:16:42 +0530

    data change

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/raash.iiee/',
+      url: 'https://www.instagram.com/raaaaashie/',
     },
     {
       name: 'Twitter',
